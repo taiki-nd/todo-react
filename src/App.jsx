72b238d2028f0a115628c6ad95490d9d7fea6b34
Taiki Noda <84260062+taiki-nd@users.noreply.github.com>
2021-10-17 06:39:58 +0900

    タスク削除機能

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,26 @@ import "./styles.css";
 
 export const App = () => {
   const [todoText, setTodoText] = useState("");
+
   const [incompleteTodoes, setIncompleteTodoes] = useState(["todo", "todo2"]);
+
   const [completeTodoes, setCompleteTodoes] = useState(["todo3"]);
+
   const onChangeTodoText = (event) => setTodoText(event.target.value);
+
   const onClickAdd = () => {
     if (todoText === "") return;
     const newTodoes = [...incompleteTodoes, todoText];
     setIncompleteTodoes(newTodoes);
     setTodoText("");
   };
+
+  const onClickDelete = (index) => {
+    const newTodoes = [...incompleteTodoes];
+    newTodoes.splice(index, 1);
+    setIncompleteTodoes(newTodoes);
+  };
+
   return (
     <>
       <div className="input_area">
@@ -26,12 +37,12 @@ export const App = () => {
       <div className="incomplete_area">
         <p className="title">未完了のToDo</p>
         <ul>
-          {incompleteTodoes.map((todo) => {
+          {incompleteTodoes.map((todo, index) => {
             return (
               <div key={todo} className="list_row">
                 <li>{todo}</li>
                 <button>完了</button>
-                <button>削除</button>
+                <button onClick={() => onClickDelete(index)}>削除</button>
               </div>
             );
           })}
