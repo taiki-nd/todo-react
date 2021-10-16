457e6ba3c184c8857021267172eb330031f59e46
Taiki Noda <84260062+taiki-nd@users.noreply.github.com>
2021-10-17 05:16:37 +0900

    JSX構造作成

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,33 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return null;
+  return (
+    <>
+      <div>
+        <input placeholder="ToDo" />
+        <button>追加</button>
+      </div>
+
+      <div>
+        <p>未完了のToDo</p>
+        <ul>
+          <div>
+            <li>todo</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+
+      <div>
+        <p>未完了のToDo</p>
+        <ul>
+          <div>
+            <li>todo</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
